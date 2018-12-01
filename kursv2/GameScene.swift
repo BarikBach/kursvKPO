112049065710371f1811cc0ea94dcc189e818b86
Артем Аксентьев <artem@macbook-pro-artem.local>
2018-12-01 16:26:52 +0300

    Промежуточная версия

--- kursv2/GameScene.swift
+++ kursv2/GameScene.swift
@@ -10,18 +10,10 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
+    private var object : SKShapeNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -35,9 +27,24 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        object = createObjects()
+        self.addChild(object!)
+        
+        
     }
-    
-    
+    func randColor() -> SKColor {
+        return SKColor.init(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: CGFloat.random(in: 0...1))
+    }
+    func createObjects() -> SKShapeNode{
+        let radius = CGFloat.random(in: 10...50)
+        let object = SKShapeNode.init(circleOfRadius: radius)
+        object.fillColor = randColor()
+        object.zPosition = 1.0
+        let x = CGFloat.random(in: -self.size.width/2 + radius ... self.size.width/2 - radius)
+        let y = CGFloat.random(in: -self.size.height/2 + radius ... self.size.height/2 - radius)
+        object.position = CGPoint(x: x, y: y)
+        return object
+    }
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -63,23 +70,27 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        print("touch")
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if  self.object != nil  {
+            object?.removeFromParent()
+            object = createObjects()
+            self.addChild(object!)
+        }
+
+       
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
+
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     
